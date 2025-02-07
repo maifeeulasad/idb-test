c2ada75c7e7223b13d3f831e626e51be8c7ed77a
DELL <kashafhossain24sahee@gmail.com>
2025-02-07 23:35:31 +0530

    bugfix: enabled decorator pattern for `idb-ts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,58 +1,49 @@
-import { defineConfig } from 'vite'
-import svgrPlugin from 'vite-plugin-svgr'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import svgrPlugin from "vite-plugin-svgr";
+import react from "@vitejs/plugin-react";
 import ViteVisualizer from "rollup-plugin-visualizer";
-import { VitePluginRadar } from 'vite-plugin-radar'
-import babel from '@rollup/plugin-babel'
-
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://stackoverflow.com/a/15802301
 const headCommitHash = (): string | undefined => {
   try {
-    return require('child_process')
-      .execSync('git rev-parse HEAD')
-      .toString();
-  } catch (_) { }
-  return undefined
-}
+    return require("child_process").execSync("git rev-parse HEAD").toString();
+  } catch (_) {}
+  return undefined;
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     __HEAD_COMMIT_HASH__: JSON.stringify(headCommitHash()),
   },
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
   build: {
-    outDir: './build',
+    outDir: "./build",
   },
-  base: './',
+  base: "./",
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [
+          ["@babel/plugin-proposal-decorators", { version: "legacy" }],
+          ["@babel/plugin-proposal-class-properties", { loose: true }],
+        ],
+      },
+    }),
     svgrPlugin({
       svgrOptions: {
         icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
       },
     }),
     VitePluginRadar({
       enableDev: true,
       analytics: {
-        id: 'G-366693052',
-        // UA-136427840-2 // <--- old one, keeping here, for documentation purpose;
+        id: "G-366693052",
       },
     }),
-    // last one is the visualizer
-    ViteVisualizer({
-      filename: './build/report-rollup-plugin-visualizer.html',
-      brotliSize: true,
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      plugins: [
-        '@babel/plugin-proposal-decorators',
-        '@babel/plugin-proposal-class-properties'
-      ]
-    })
+    // ViteVisualizer({
+    //   filename: "./build/report-rollup-plugin-visualizer.html",
+    //   brotliSize: true,
+    // }),
   ],
-})
+});
